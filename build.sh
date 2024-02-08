59c88da50c38ab1ba8c874631af8089e6f2d3ab2
camelStyleUser <124447863+camelStyleUser@users.noreply.github.com>
2024-02-08 18:40:26 +0200

    comment

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ cp -r sprites/ mod/sprites/
 cp -r bundles/ mod/bundles/
 cp -r sprites-override/ mod/sprites-override/
 cd mod
+#assembling all files
 for i in *.asm
 do
     if test -f "$i"
@@ -17,12 +18,15 @@ do
        rm "$i"
     fi
 done
+#removing unneeded files
 rm jclass.inc
 rm bytecode.inc
 cd ..
 cp mod.json mod/
 cd mod
+#get files for d8
 ls .|grep ".class">list.txt
+#d8 them all
 d8 *.class --main-dex-list list.txt
 rm list.txt
 echo "zipping it into the jar"
